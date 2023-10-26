c22386dfcede9a7895a7d4b10bacea515c6fb2c4
mafioznik <kiselev.danil201313@gmail.com>
2023-10-26 18:49:50 +0300

    new values

--- ThetaNeurons/main.py
+++ ThetaNeurons/main.py
@@ -549,19 +549,25 @@ def stretching_tau(delta_tau: float, tau_final: float, times: int, start_string_
 if __name__ == '__main__':
     #default_theta_neuron(50, 0.5, 0.8, 1.5, 0.04, 5, 750, 2000)
     #visualize_change('tau', 'noNoise/results_tau_negative.csv', 57, 5000, 5000, 0.001)
-    #visualize_data('Noise/results_tau_negative.csv', 50, 1000, 0.001)
+    #visualize_data('suspect_v2.csv', 440, 8000, )
 
     if click.confirm(f'[__main__] Run stretching?', default=False):
         print(f'[Multiprocessing] {multiprocessing.cpu_count()} cores available')
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
         result_eta = pool.starmap_async(stretching_eta, [(-0.01, 0, 2000, -1, 'Noise/results_eta_negative.csv', 0.001,),
-                                                     (0.01, 20, 2000, -1, 'Noise/results_eta_positive.csv', 0.001,)])
+                                                         (0.01, 10, 2000, -1, 'Noise/results_eta_positive.csv', 0.001,),
+                                                         (-0.01, 0, 2000, -1, 'noNoise/results_eta_negative.csv', 0,),
+                                                         (0.01, 10, 2000, -1, 'noNoise/results_eta_positive.csv', 0,)
+                                                         ])
         result_kappa = pool.starmap_async(stretching_kappa, [(-0.01, 0, 2000, -1, 'Noise/results_kappa_negative.csv', 0.001,),
-                                                         (0.01, 20, 2000, -1, 'Noise/results_kappa_positive.csv', 0.001,)])
+                                                             (0.01, 10, 2000, -1, 'Noise/results_kappa_positive.csv', 0.001,),
+                                                             (-0.01, 0, 2000, -1, 'noNoise/results_kappa_negative.csv',0,),
+                                                             (0.01, 10, 2000, -1, 'noNoise/results_kappa_positive.csv', 0,)
+                                                             ])
         result_tau = pool.starmap_async(stretching_tau, [(-0.01, 0, 2000, -1, 'Noise/results_tau_negative.csv', 0.001,),
-                                                     (0.01, 20, 2000, -1, 'Noise/results_tau_positive.csv', 0.001,),
-                                                     (0.01, 20, 2000, -1, 'noNoise/results_tau_positive.csv', 0,),
-                                                     (-0.01, 0, 2000, -1, 'noNoise/results_tau_negative.csv', 0,)])
+                                                     (0.01, 10, 2000, -1, 'Noise/results_tau_positive.csv', 0.001,),
+                                                     (-0.01, 0, 2000, -1, 'noNoise/results_tau_negative.csv', 0,),
+                                                     (0.01, 10, 2000, -1, 'noNoise/results_tau_positive.csv', 0,)])
         result_eta.get()
         result_kappa.get()
         result_tau.get()
