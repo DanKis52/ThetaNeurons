4b280576a66835a3377b39fd2b56014f463f41e5
Dan <kiselev.danil201313@gmail.com>
2023-10-26 23:02:25 +0300

    cosmetic change

--- ThetaNeurons/main.py
+++ ThetaNeurons/main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     #default_theta_neuron(50, 0.5, 0.8, 1.5, 0.04, 5, 750, 2000)
     #visualize_change('tau', 'noNoise/results_tau_negative.csv', 57, 5000, 5000, 0.001)
     #visualize_data('SuspectResults/suspect_kappa.csv', 10, 20000, 0)
-
+    suspect_results('suspect_v2.csv')
     if click.confirm(f'[__main__] Run stretching?', default=False):
         print(f'[Multiprocessing] {multiprocessing.cpu_count()} cores available')
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
